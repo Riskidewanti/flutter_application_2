012014de247b54cdfd780e25d76461bd94e58caf
Riski Dewanti <dewantiriski@gmail.com>
2026-05-04 18:42:44 +0700

    update package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.riski.taskmate"
+    namespace = "com.ti24a5.app26"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -29,7 +29,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.riski.taskmate"
+        applicationId = "com.ti24a5.app26"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
--- android/app/src/main/kotlin/com/ti24a5/app26/MainActivity.kt
+++ android/app/src/main/kotlin/com/ti24a5/app26/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.riski.taskmate
+package com.ti24a5.app26
 
 import io.flutter.embedding.android.FlutterActivity
 
